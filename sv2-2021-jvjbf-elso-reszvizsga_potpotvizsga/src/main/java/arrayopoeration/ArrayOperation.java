8b7ad18f1a95c7df57c6fbd31a580fff64bbe6b8
vargajani86hun <jani.varga86@gmail.com>
2021-12-30 14:57:31 +0100

    Task ArrayOperation is ready.

---
+++ sv2-2021-jvjbf-elso-reszvizsga_potpotvizsga/src/main/java/arrayopoeration/ArrayOperation.java
@@ -0,0 +1,14 @@
+package arrayopoeration;
+
+public class ArrayOperation {
+    public String getWordsBackwards(String[] words) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+}
