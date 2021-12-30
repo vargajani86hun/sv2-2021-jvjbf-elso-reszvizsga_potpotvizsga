39a1e18def4a98a7d9dac07d11e2628b5ec5a415
vargajani86hun <jani.varga86@gmail.com>
2021-12-30 15:04:14 +0100

    Task Absolute is ready.

---
+++ sv2-2021-jvjbf-elso-reszvizsga_potpotvizsga/src/main/java/absolute/Absolute.java
@@ -0,0 +1,13 @@
+package absolute;
+
+public class Absolute {
+    public boolean isAbsolute(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+}
